d61b2919af569c0f7e74f995f77ac13f3111c4fb
favor <favoriteigo0904@gmail.com>
2026-08-17 09:34:31 +0900

    katago実行ファイルをOSによって切り替えられるように修正

--- config.py
+++ config.py
@@ -1,9 +1,13 @@
 import os
+import platform
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # KataGo settings
-KATAGO_PATH = os.path.join(BASE_DIR, "katago", "katago.exe")
+if platform.system() == "Windows":
+    KATAGO_PATH = os.path.join(BASE_DIR, "katago", "katago.exe")
+else:
+    KATAGO_PATH = os.path.join(BASE_DIR, "katago", "katago")
 MODEL_PATH = os.path.join(BASE_DIR, "katago", "kata1-b10c128-s41138688-d27396855.txt.gz")
 CONFIG_PATH = os.path.join(BASE_DIR, "katago", "default_gtp.cfg")
 SGF_DIR = os.path.join(BASE_DIR, "sgf")

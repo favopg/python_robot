d3344cba7fe2e039f48fb8e10d01f66fa8773e50
favor <favoriteigo0904@gmail.com>
2026-08-17 17:12:22 +0900

    katagoのモデル修正

--- config.py
+++ config.py
@@ -8,6 +8,6 @@ if platform.system() == "Windows":
     KATAGO_PATH = os.path.join(BASE_DIR, "katago", "katago.exe")
 else:
     KATAGO_PATH = os.path.join(BASE_DIR, "katago", "katago")
-MODEL_PATH = os.path.join(BASE_DIR, "katago", "kata1-b28c512nbt-s7168446720-d4316919285.bin.gz")
+MODEL_PATH = os.path.join(BASE_DIR, "katago", "kata1-b18c384nbt-s9996604416-d4316597426.bin.gz")
 CONFIG_PATH = os.path.join(BASE_DIR, "katago", "default_gtp.cfg")
 SGF_DIR = os.path.join(BASE_DIR, "sgf")
